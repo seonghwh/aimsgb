52ead6a81c4e660ea7c8c655e92de68163e4601f
Seonghwan Hong <89226363+seonghwh@users.noreply.github.com>
2024-03-19 06:12:03 +0000

    combined converter.py and GB generator

--- BTO GB.py
+++
@@ -1,9 +0,0 @@
-#%%
-from aimsgb import GrainBoundary, Grain
-
-#%%
-s_input = Grain.from_mp_id("mp-5229")
-#%%
-gb = GrainBoundary([1, 1, 0], 3, [1, -1, 2], s_input)
-structure = Grain.stack_grains(gb.grain_a, gb.grain_b, direction=gb.direction)
-# %%
